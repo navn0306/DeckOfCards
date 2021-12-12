48f9613d30a50c6ed6eb1e9e95d2a87cb087129f
Navneet <navn0306@gmail.com>
2021-12-12 17:27:38 +0530

    ADD: Welcome message for program

---
+++ src/com/bridge/deckOfCards/DeckOfCards.java
@@ -0,0 +1,12 @@
+package com.bridge.deckOfCards;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome , this program initializes deck of cards having suit\n" +
+                "(\"Clubs\", \"Diamonds\", \"Hearts\", \"Spades\") & Rank (\"2\", \"3\", \"4\", \"5\", \"6\", \"7\", \"8\",\n" +
+                "\"9\", \"10\", \"Jack\", \"Queen\", \"King\", \"Ace\"). Shuffles the cards using Random\n" +
+                "method and then distribute 9 Cards to 4 Players and Prints the Cards received by\n" +
+                "the 4 Players using 2D Array...");
+
+    }
+}
